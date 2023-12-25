82f7d6b81b640606c75623c56a09824823527d94
--replace-all <h.buterak@gmail.com>
2023-12-25 13:55:28 +0200

    feat: implement visualizing payments on dashboard chart
    updates the existing chart with real payments data

--- src/containers/dashboard/DashboardChart.jsx
+++ src/containers/dashboard/DashboardChart.jsx
@@ -1,26 +1,93 @@
 import { useState } from 'react'
 import LineChart from '../../components/LineChart'
-import { weeklyData, monthData, annualData } from './Data'
 import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const ChartDashboard = () => {
-  const [viewMode, setViewMode] = useState('daily')
+  const [viewMode, setViewMode] = useState('week')
+  const [chartData, setChartData] = useState({})
+
+  
 
   const handleViewModeChange = (mode) => {
     setViewMode(mode)
   }
 
-  const getChartData = () => {
-    if (viewMode === 'weekly') {
-      return weeklyData
-    } else if (viewMode === 'monthly') {
-      return monthData
-    } else {
-      return annualData
+  const getChartData = async () => {
+    if (viewMode === 'week') {
+      try {
+        const response = await axios.get(`${process.env.VITE_APP_API_URL}/payment/chartinfo?week=true`)
+        const weekPayments = {
+          labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
+          datasets: [
+            {
+              label: 'Money paid',
+              data: response.data.data.length > 0 ? response.data.data.map(data => {
+                return data.totalAmount;
+              }) : [],
+              borderColor: '#165F75',
+              backgroundColor: 'rgba(59, 130, 246, 0.2)'
+            }
+          ]
+        }       
+        setChartData(weekPayments)
+      } catch (error) {
+        console.log(error);
+      } 
+    } else if (viewMode === 'month') {
+      try {
+        let todayDate = new Date();
+
+        const response = await axios.get(`http://localhost:3000/api/v2/payment/chartinfo?month=${todayDate.getMonth() +1 }&year=${todayDate.getFullYear()}`)
+        const monthPayments = {
+          labels: response.data.data.length > 0 ? response.data.data.map(data => {
+            return `${data.day}`;
+          }) : [],
+          datasets: [
+            {
+              label: 'Money paid',
+              data: response.data.data.length > 0 ? response.data.data.map(data => {
+                return data.totalAmount;
+              }) : [],
+              borderColor: '#165F75',
+              backgroundColor: 'rgba(59, 130, 246, 0.2)'
+            }
+          ]
+        }       
+        setChartData(monthPayments)
+      } catch (error) {
+        console.log(error);
+      }
+    } else if (viewMode === 'year') {
+      try {
+        let todayDate = new Date();
+
+        const response = await axios.get(`http://localhost:3000/api/v2/payment/chartinfo?year=${todayDate.getFullYear()}`)
+        const yearPayments = {
+          labels: response.data.data.length > 0 ? response.data.data.map(data => {
+            return `${data.month}`;
+          }) : [],
+          datasets: [
+            {
+              label: 'Money paid',
+              data: response.data.data.length > 0 ? response.data.data.map(data => {
+                return data.totalAmount;
+              }) : [],
+              borderColor: '#165F75',
+              backgroundColor: 'rgba(59, 130, 246, 0.2)'
+            }
+          ]
+        }       
+        setChartData(yearPayments)
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
-
-  const chartData = getChartData()
+  useEffect(() => {
+    getChartData()    
+  }, [viewMode])
 
   const { isOpen } = useSelector((state) => state.sidebar)
 
@@ -32,47 +99,48 @@ const ChartDashboard = () => {
           : 'flex flex-col gap-4'
       } mb-8`}
     >
-      <div
+      <div className="border w-full rounded-lg shadow-md p-6 bg-white">
+        <h2 className="text-xl font-semibold mb-4">
+          {viewMode === 'week'
+            ? `This week's Collections`
+            : viewMode === 'month'
+              ? `This month's Collections`
+              : `This year's Collections`}
+        </h2>
+        <div className=" rounded p-4 min-h-[500px] max-h-[500px] font-bold">
+          {Object.keys(chartData).length > 0 ? <LineChart className="w-full" data={chartData} />: ""}
+          
+        </div>
+        <div
         className={`${
           isOpen ? 'flex items-center gap-6' : 'flex flex-col gap-4'
         }`}
       >
         <button
           className={`px-2 py-2 rounded ${
-            viewMode === 'weekly' ? 'bg-primary text-white' : 'bg-gray-200'
+            viewMode === 'week' ? 'bg-primary text-white' : 'bg-gray-200'
           }`}
-          onClick={() => handleViewModeChange('weekly')}
+          onClick={() => handleViewModeChange('week')}
         >
-          View Weekly Collections
+          Week's Collections
         </button>
         <button
           className={`px-2 py-2 rounded ${
-            viewMode === 'monthly' ? 'bg-primary text-white' : 'bg-gray-200'
+            viewMode === 'month' ? 'bg-primary text-white' : 'bg-gray-200'
           }`}
-          onClick={() => handleViewModeChange('monthly')}
+          onClick={() => handleViewModeChange('month')}
         >
-          View Monthly Collections
+          Month's Collections
         </button>
         <button
           className={`px-2 py-2 rounded ${
-            viewMode === 'annual' ? 'bg-primary text-white' : 'bg-gray-200'
+            viewMode === 'year' ? 'bg-primary text-white' : 'bg-gray-200'
           }`}
-          onClick={() => handleViewModeChange('annual')}
+          onClick={() => handleViewModeChange('year')}
         >
-          View Annual Collections
+          Year's Collections
         </button>
       </div>
-      <div className="border w-full rounded-lg shadow-md p-6 bg-white">
-        <h2 className="text-xl font-semibold mb-4">
-          {viewMode === 'daily'
-            ? 'Daily Collections'
-            : viewMode === 'monthly'
-              ? 'Monthly Collections'
-              : 'Annual Collections'}
-        </h2>
-        <div className="border rounded p-4 min-h-[500px] max-h-[500px] font-bold">
-          <LineChart className="w-full" data={chartData} />
-        </div>
       </div>
     </div>
   )
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    jsxInject: `import React from 'react';`,
-  },
+
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env': env
+    },
+    plugins: [react()],
+    esbuild: {
+      jsxInject: `import React from 'react';`,
+    },
+  }
 })
